c38d1088b2ecf24ee7475b961db002a9be6795c6
Tthevvi <vikadamchat12@gmail.com>
2026-07-29 23:01:40 +0700

    Кастомная модель пользователя Cashier с ролями

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Cashier
 
-# Register your models here.
+
+@admin.register(Cashier)
+class CashierAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Роль", {"fields": ("role",)}),
+    )
+    list_display = ("username", "get_full_name", "role", "is_staff")
